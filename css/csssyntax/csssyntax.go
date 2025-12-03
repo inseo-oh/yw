768ba2f28565a22f2d6a2ae1b426c072c6983ba3
Oh Inseo <dhdlstjtr@gmail.com>
2025-12-03 15:19:19 +0900

    [css/csssyntax] Make consumeSimpleBlockWith return non-pointer value

--- css/csssyntax/csssyntax.go
+++ css/csssyntax/csssyntax.go
@@ -988,19 +988,19 @@ func (ts *tokenStream) consumeIdentTokenWith(i string) error {
 	}
 	return nil
 }
-func (ts *tokenStream) consumeSimpleBlockWith(tp simpleBlockType) (*simpleBlockToken, error) {
+func (ts *tokenStream) consumeSimpleBlockWith(tp simpleBlockType) (simpleBlockToken, error) {
 	oldCursor := ts.cursor
 	tk, err := ts.consumeTokenWith(tokenTypeSimpleBlock)
 	if err != nil {
-		return nil, err
+		return simpleBlockToken{}, err
 	}
 	blk := tk.(simpleBlockToken)
 	if blk.tp != tp {
 		// TODO: Describe what token we want in more friendly way.
 		ts.cursor = oldCursor
-		return nil, fmt.Errorf("expected simple block with type %v", tp)
+		return simpleBlockToken{}, fmt.Errorf("expected simple block with type %v", tp)
 	}
-	return &blk, nil
+	return blk, nil
 }
 func (ts *tokenStream) consumeAstFuncWith(name string) *astFuncToken {
 	oldCursor := ts.cursor
