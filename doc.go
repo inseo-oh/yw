0cf314e95e0be307d223afde76b0354b42b9a948
Oh Inseo <dhdlstjtr@gmail.com>
2025-12-08 16:09:55 +0900

    Refactor: Move main executable code to cmd directory

--- doc.go
+++
@@ -1,9 +0,0 @@
-// This file is part of YW project. Copyright 2025 Oh Inseo (YJK)
-// SPDX-License-Identifier: BSD-3-Clause
-// See LICENSE for details, and LICENSE.thirdparty.md for third-party license information.
-
-// yw is a web page rendering engine aimed at simplicity.
-//
-// The main executable package only implements small fontend for rest of the engine.
-// For rest of it, look at one of subpackages.
-package main
